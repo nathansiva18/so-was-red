a5c3c2ed2f580061c27f94e63412bab12f1e8c65
nathansiva18 <nathansiva018@gmail.com>
2022-12-03 20:59:42 +0530

    first ng methods

---
+++ src/test/java/org/ng/ClassA.java
@@ -0,0 +1,14 @@
+package org.ng;
+
+public class ClassA {
+	private void johnwick() {
+		System.out.println("keanu reevs");
+	}
+	private void matrix() {
+		System.out.println("siva");
+	}
+	private void parabellum() {
+		System.out.println("matrix");
+	}
+
+}
